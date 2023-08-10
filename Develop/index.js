974ffdd248c863b2ea3af8f5b78ee828d967a867
notUnEeVeN <tybalt.mallet@gmail.com>
2023-08-10 12:47:12 -0700

    added the requirements and the questions using inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description for your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Installation instructions:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage information:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Contribution guidelines:',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Test instructions:',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your application:',
+        name: 'license',
+        choices: ['MIT', 'ISC', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        message: 'Enter your Github username:',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
